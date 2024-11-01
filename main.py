d161c4b3876649940390f98308e6d8188201cd43
samay90 <samaypatel90@gmail.com>
2024-11-02 00:07:53 +0530

    More Features

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import os
 if not os.path.exists("dbms/notes.csv"):
     f = open("dbms/notes.csv","w")
     f.close()
-st.set_page_config(page_title="Anonymous Notes Maker", page_icon="📝")
+st.set_page_config(initial_sidebar_state="collapsed",page_title="Anonymous Notes Maker", page_icon="📝")
 st.title("📝 Anonymous Notes Maker")
-
 st.header("How to use ?")
 st.write("""
 1) Just type title and body of your notes
@@ -14,3 +13,4 @@ st.write("""
 """)
 st.header("Create your notes today!!")
 st.page_link("pages/Create.py",label="Create Note",icon="📝")
+st.page_link("pages/Find Note.py",label="Find Note",icon="🔍")
--- pages/Find Note.py
+++ pages/Find Note.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Find Note")
-id = st.text_input("Id",key="id",placeholder="Id")
+id = st.text_input("Note Id",key="id",placeholder="Enter note id")
 password = st.text_input("Password",key="pass",placeholder="Password")
 if (not(id==""or password=="")):
     st.link_button("Get Note",f"/Note?id={id}&password={password}")
